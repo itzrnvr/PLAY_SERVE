c4da40d6c3d0ed6aa0870bfee868a72f664b0dd3
ranveer <sanskar9797@gmail.com>
2024-02-25 20:33:38 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+    return <View className='bg-red-500 h-full'>
+
+    </View>
+}
+
+export default App;
+
+//npx react-native run-ios --simulator='iPhone 15 Pro'          
