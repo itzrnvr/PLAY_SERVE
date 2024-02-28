ae05dcfd47e15336d11182840622ca213d94fa4a
ranveer <sanskar9797@gmail.com>
2024-02-28 12:43:19 +0530

    feat: Add static server for serving static assets
    This commit adds a static server to the app that can be used to serve static assets, such as images, CSS, and JavaScript files. The server is started on port 8080 and can be accessed by visiting http://localhost:8080/index.html.
    
    This feature will allow us to serve static assets from the app itself, which will improve performance and reduce the need for external dependencies.

--- App.jsx
+++ App.jsx
@@ -1,12 +1,34 @@
-import react from 'react';
-import { Text, View } from 'react-native';
+import react, {useEffect} from 'react';
+import {Text, View} from 'react-native';
+import StaticServer from 'react-native-static-server';
+import RNFS from 'react-native-fs';
+import {WebView} from 'react-native-webview';
 
 const App = () => {
-    return <View className='bg-red-500 h-full'>
+  useEffect(() => {
+    // Path to the folder containing static files
+    let path = RNFS.MainBundlePath + '/static_assets';
 
+    // Creating a new server instance on port 8080
+    let server = new StaticServer(8080, path);
+
+    // Starting the server
+    server.start().then(url => {
+      console.log('Server started at ' + url);
+    });
+  });
+
+  return (
+    <View style={{flex: 1}}>
+      <WebView
+        source={{
+          uri: 'http://localhost:8080/index.html',
+        }}
+      />
     </View>
-}
+  );
+};
 
 export default App;
 
-//npx react-native run-ios --simulator='iPhone 15 Pro'          
+//npx react-native run-ios --simulator='iPhone 15 Pro'
